c39738fd67a75636425d2ef55771f3b46d6566b1
andrewjrvhunt <andrzej@ahunt.org>
2009-11-24 15:24:45 +0000

    Added data checking to BoatInfo. Updated formatting (generally).

--- src/org/ahunt/simpleRowLog/common/BoatInfo.java
+++ src/org/ahunt/simpleRowLog/common/BoatInfo.java
@@ -17,16 +17,21 @@
  *
  *
  *	Changelog:
+ *  24/11/2009: Cleaned up the documentation and added an exception for the
+ *  			Constructor. Added data checking.
  *	23/08/2009:	Changelog added.
  */
 
 package org.ahunt.simpleRowLog.common;
 
 /**
- * All the information for a specific group.
+ * All the information for a specific boat. This is an immutable object: data
+ * can only be modified by directly addressing the database. Only the database
+ * implementation should be creating these.
  * 
  * @author Andrzej JR Hunt
- * @version 0.01 - 21. September 2008
+ * @version 0.02 - 24. November 2008
+ * @see org.ahunt.simpleRowLog.interfaces.Database
  */
 public class BoatInfo {
 
@@ -35,7 +40,7 @@ public class BoatInfo {
 
 	/** Type of Boat. E.g. 4x, 8+... */
 	private String type;
-	
+
 	/** Whether or nor the boat is in the boathouse. */
 	private boolean inHouse;
 
@@ -43,13 +48,29 @@ public class BoatInfo {
 	 * Create the information for a Boat.
 	 * 
 	 * @param name
-	 *            The name of the group.
+	 *            The name of the boat. Cannot be null or empty.
 	 * @param type
-	 *            The type of boat, in whatever style the end user wants.
+	 *            The type of boat, in whatever style the end user wants. This
+	 *            could potentially be used for categorisation of boats, so it
+	 *            would be good to enforce a pattern in the UI.
+	 * @param inHouse
+	 *            Whether or not the boat is in the current boathouse. This
+	 *            determines whether a boat is "available", i.e. visible for
+	 *            selection.
+	 * @throws IllegalArgumentException
+	 *             If the data is supplied incorrectly.
 	 */
-	public BoatInfo(String name, String type, boolean inHouse) {
+	public BoatInfo(String name, String type, boolean inHouse)
+			throws IllegalArgumentException {
+		if (name == null | name.length() == 0) { // Check that the boat has a name.
+			throw new IllegalArgumentException("Name of boat must be > 1.");
+		}
 		this.name = name;
-		this.type = type;
+		if (type.length() != 0) { //If there is a string or is null assign it.
+			this.type = type;
+		} else { // But for zero length set to null.
+			this.type = null;
+		}
 		this.inHouse = inHouse;
 	}
 
@@ -65,14 +86,15 @@ public class BoatInfo {
 	/**
 	 * Get the type of boat.
 	 * 
-	 * @return The type of boat.
+	 * @return The type of boat. null if no type is defined.
 	 */
 	public String getType() {
 		return type;
 	}
-	
+
 	/**
 	 * Check whether the boat is in the boathouse.
+	 * 
 	 * @return Whether the boat is in the boathouse.
 	 */
 	public boolean getInHouse() {
